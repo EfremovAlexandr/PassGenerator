e069241d8a1c9539fb80ecda62550b1e8465f286
888 <efremov.vrn@gmail.com>
2022-04-24 13:52:21 +0300

    ДЗ выполнено

--- passgen/generator/views.py
+++ passgen/generator/views.py
@@ -6,11 +6,20 @@ import random
 def home(request):
     return render(request, 'passgen/home.html')
 
+def information(request):
+    return render(request, 'passgen/information.html')
 
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    lenght = 10
+    lenght = int(request.GET.get('lenght', 12))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('#$%^&'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
 
     thepassword = ''
     for x in range(lenght):
